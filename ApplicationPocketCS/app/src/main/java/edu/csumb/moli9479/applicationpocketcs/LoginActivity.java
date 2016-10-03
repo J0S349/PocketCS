44521129c7d14b50d69582d9a55da3d1dfe8ca11
CrownedGuardian <stephenenegron@student.hartnell.edu>
2016-10-02 17:33:39 -0700

    Added facebook login functionality, implemented login, mainactivity and added fb button
    Summary:
    Adding splash screen
    
    Adding splash screen and resolving merge conflicts
    
    Removing unused packages
    
    Test Plan: Splash screen is working
    
    Reviewers: JoeM88
    
    Reviewed By: JoeM88
    
    Subscribers: NathanielDavidson, sorenlassen-csumb
    
    Differential Revision: https://reviews.facebook.net/D64575

--- ApplicationPocketCS/app/src/main/java/edu/csumb/moli9479/applicationpocketcs/LoginActivity.java
+++ ApplicationPocketCS/app/src/main/java/edu/csumb/moli9479/applicationpocketcs/LoginActivity.java
@@ -4,17 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.Profile;
-import com.facebook.ProfileTracker;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
