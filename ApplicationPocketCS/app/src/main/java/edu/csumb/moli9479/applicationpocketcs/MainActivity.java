2a1499e045e8f58cee3ff7e47648476654fc409f
Joseph Molina <jomolina@csumb.edu>
2016-10-08 19:56:31 -0700

    Added navigation drawer files, added app icons, changed the app's main icon

--- ApplicationPocketCS/app/src/main/java/edu/csumb/moli9479/applicationpocketcs/MainActivity.java
+++ ApplicationPocketCS/app/src/main/java/edu/csumb/moli9479/applicationpocketcs/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.csumb.moli9479.applicationpocketcs;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import com.facebook.AccessToken;
@@ -13,12 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView firstnameView,userId;
     private ProfilePictureView profilepic;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creating the Navigation Drawer.
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Setting the fields with their respect values.
         firstnameView = (TextView) findViewById(R.id.firstname);
         userId = (TextView) findViewById(R.id.userId);
         profilepic = (ProfilePictureView) findViewById(R.id.image);
@@ -55,4 +67,17 @@ public class MainActivity extends AppCompatActivity {
         LoginManager.getInstance().logOut();
         goLoginScreen();
     }
+
+    /*******************************************************************************
+     Method for the hamburger icon, if the item clicked is the hamburger icon
+     the navigation will appear, else it will be hidden.
+     *******************************************************************************/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
