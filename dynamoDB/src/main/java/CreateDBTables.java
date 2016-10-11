a8ee55192d3ca48bbf2e9b8689291e5fc861f3ad
Jose Sanchez-Garcia <josebsan96@gmail.com>
2016-10-11 13:43:38 -0700

    Created the classes that would be used to create tables within the database

---
+++ dynamoDB/src/main/java/CreateDBTables.java
@@ -0,0 +1,245 @@
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.TableCollection;
+import com.amazonaws.services.dynamodbv2.document.internal.IteratorSupport;
+import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.amazonaws.services.s3.model.Region;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by Peeps on 10/9/16.
+ */
+
+public class CreateDBTables {
+    // This function will create all the tables
+    public static void initialize(){
+        createAlgorithmsAndDataStructuresTable("Algorithms");
+        createAlgorithmsAndDataStructuresTable("Data Structures");
+    }
+
+    private static void createAlgorithmsAndDataStructuresTable(String tableName){
+        //checks to see whether the tables already exist or not
+        if(tableAlreadyExist(tableName))
+        {
+            return;
+        }
+
+        ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+
+        attributeDefinitions.add(new AttributeDefinition()
+                                    .withAttributeName("ID")
+                                    .withAttributeType(ScalarAttributeType.N)); // makes a ID column of type Number (int)
+
+        // makes a name column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("name")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes a description column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("description")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes a UserID column of type int
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("userID")
+                .withAttributeType(ScalarAttributeType.N));
+        // Makes a categoryID column of type int
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("categoryID")
+                .withAttributeType(ScalarAttributeType.N));
+        // Makes a runtime column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("runtime")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes a image column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("image")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes a helpfulLink column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("helpfulLink")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes dateCreated column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("dateCreated")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes dateUpdated column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("dateUpdated")
+                .withAttributeType(ScalarAttributeType.S));
+
+        // Create the KeySchema for knowing what is primary key(s) of the table
+        ArrayList<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
+        keySchema.add(new KeySchemaElement()
+                        .withAttributeName("ID")
+                        .withKeyType(KeyType.HASH));    // Partition Key
+
+        // Now we create a table request so that DynamoDB know that we want to create a table
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName(tableName)
+                .withKeySchema(keySchema)
+                .withAttributeDefinitions(attributeDefinitions)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                                            .withReadCapacityUnits(5L)
+                                            .withWriteCapacityUnits(6L));
+
+        // Now we create the reques to create the table.
+        try {
+            DBConnector.getDynamoDB().createTable(request);
+            System.out.println();
+            } catch (Exception e){
+            System.out.println("There was an issue creating the table " + tableName);
+            e.printStackTrace();
+        }
+    }
+
+    // This is used to create category tables
+    private static void createCategoryTables(String tableName){
+        ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("ID")
+                .withAttributeType(ScalarAttributeType.N)); // makes a ID column of type Number (int)
+
+        // makes a name column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("name")
+                .withAttributeType(ScalarAttributeType.S));
+        // Makes a description column of type String
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("description")
+                .withAttributeType(ScalarAttributeType.S));
+
+        // Create the KeySchema for knowing what is primary key(s) of the table
+        ArrayList<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
+        keySchema.add(new KeySchemaElement()
+                .withAttributeName("ID")
+                .withKeyType(KeyType.HASH));    // Partition Key
+
+        // Now we create a table request so that DynamoDB know that we want to create a table
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName(tableName)
+                .withKeySchema(keySchema)
+                .withAttributeDefinitions(attributeDefinitions)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+        // Now we create the request to create the table.
+        try {
+            DBConnector.getDynamoDB().createTable(request);
+            System.out.println();
+        } catch (Exception e){
+            System.out.println("There was an issue creating the table " + tableName);
+            e.printStackTrace();
+        }
+    }
+
+    // Create the user table
+    private static void createUserTable(){
+        ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("ID")
+                .withAttributeType(ScalarAttributeType.N)); // makes a ID column of type Number (int)
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("firstName")
+                .withAttributeType(ScalarAttributeType.S));
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("lastName")
+                .withAttributeType(ScalarAttributeType.S));
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("email")
+                .withAttributeType(ScalarAttributeType.S));
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName("facebookID")
+                .withAttributeType(ScalarAttributeType.S));
+
+        // Create the KeySchema for knowing what is primary key(s) of the table
+        ArrayList<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
+        keySchema.add(new KeySchemaElement()
+                .withAttributeName("ID")
+                .withKeyType(KeyType.HASH));    // Partition Key
+
+        // Now we create a table request so that DynamoDB know that we want to create a table
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName("user")
+                .withKeySchema(keySchema)
+                .withAttributeDefinitions(attributeDefinitions)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+
+        // Now we create the reques to create the table.
+        try {
+            DBConnector.getDynamoDB().createTable(request);
+            System.out.println();
+        } catch (Exception e){
+            System.out.println("There was an issue creating the table user");
+            e.printStackTrace();
+        }
+    }
+
+    // This function will check whether
+    private static boolean tableAlreadyExist(String tableName){
+        TableCollection<ListTablesResult> tables = DBConnector.getDynamoDB().listTables();
+        Iterator<Table> iterator = tables.iterator();
+
+        while(iterator.hasNext()) {
+            Table table = iterator.next();
+            if (table.getTableName().equals(tableName)) //checks whether the table being created is inside the database
+            {
+                return true;
+            }
+        }
+        return false; // table doesn't exist
+    }
+
+    // Adding a row to any type of category table (dataStructuresCategory, algorithmsCategory, softwareDesignPatternsCategory)
+    private static boolean addCategoryToTable(String tableName, int primaryKey, String name, String description)
+    {
+        Table table = DBConnector.getDynamoDB().getTable(tableName);
+
+        // Try to add into the table
+        try{
+            Item sessionRow = new Item()
+                    .withPrimaryKey("ID", primaryKey)
+                    .withString("name", name)
+                    .withString("description", description); //creating a session row to add to table
+            table.putItem(sessionRow);
+            return true;
+        } catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Adding a user to the table
+    private static boolean addUser(int ID, String firstName, String lastName, String email, String facebookID)
+    {
+        Table table = DBConnector.getDynamoDB().getTable("user");
+        try{
+            Item newUser = new Item()
+                    .withPrimaryKey("ID", ID)
+                    .withString("firstName", firstName)
+                    .withString("lastName", lastName)
+                    .withString("email", email)
+                    .withString("facebookID", facebookID);
+            table.putItem(newUser);
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+}
---
+++ dynamoDB/src/main/java/DBConnector.java
@@ -0,0 +1,44 @@
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import com.amazonaws.services.s3.model.Region;
+
+/**
+ * Created by Peeps on 10/8/16.
+ *  This is the class that will allow us to comunicate between the database and our application
+ *
+ */
+public class DBConnector {
+
+    private static DynamoDB dynamoDB;
+    private static AmazonDynamoDB client;
+
+    public static void initialize(){
+        String access = System.getProperty("access");
+        String secret = System.getProperty("secret");
+
+        // This will create a new instance of the object in case that it is not created yet
+        if(access == null || secret == null || access.equals("") || secret.equals("")){
+            client = new AmazonDynamoDBClient(new BasicAWSCredentials("access", "secret"));
+            // connect to local dynamoDB server
+            client.setEndpoint("http://localhost:8080");
+        }
+        else {  // This will just set the client object to connect to the dynamo database
+            client = new AmazonDynamoDBClient(new BasicAWSCredentials(access, secret));
+            client.setRegion(Region.US_West.toAWSRegion());
+        }
+        dynamoDB = new DynamoDB(client);
+    }
+
+    public static void close(){
+        dynamoDB.shutdown();
+        client.shutdown();
+    }
+
+    // returns the dynamo DB instance. Singletons software design pattern
+    public static DynamoDB getDynamoDB(){return dynamoDB; } //get dynamoDB instance
+
+}
