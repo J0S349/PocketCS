e6a6164c260b638bd2df60fe9a52c437a5783d62
Joseph Molina <jomolina@csumb.edu>
2016-10-26 19:01:29 -0700

    Modified the navigation drawer to include, about and logout button, added profile activity
    Summary: Modified the navigation drawer to include, "Profile","About" screens and the logout button
    
    Test Plan: Check that my code meets proper syntax.
    
    Reviewers: CrownedGuardian
    
    Reviewed By: CrownedGuardian
    
    Differential Revision: http://csumbse.net/D91

--- ApplicationPocketCS/app/src/main/java/edu/csumb/moli9479/applicationpocketcs/AboutActivity.java
+++ ApplicationPocketCS/app/src/main/java/edu/csumb/moli9479/applicationpocketcs/AboutActivity.java
@@ -9,5 +9,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
