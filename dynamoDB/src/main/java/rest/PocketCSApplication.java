f170c5c91fbbe1c51f6f55457b9fbc495f7e30d5
Jose Sanchez-Garcia <josebsan96@gmail.com>
2016-10-26 19:03:32 -0700

    Created the AlgorithmTable Class as well as working on setting up the connection to DynamoDB instance on AWS
    Summary: Pop
    
    Test Plan: Setting up DynamoDB connection
    
    Reviewers: JoeM88, CrownedGuardian, zapa4234
    
    Differential Revision: http://csumbse.net/D89

---
+++ dynamoDB/src/main/java/rest/PocketCSApplication.java
@@ -0,0 +1,12 @@
+package rest;
+
+import javafx.application.Application;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by Peeps on 10/26/16.
+ */
+@ApplicationPath("rest")
+public abstract class PocketCSApplication extends Application {
+
+}
---
+++ dynamoDB/src/main/java/rest/PocketCSResource.java
@@ -0,0 +1,8 @@
+package rest;
+
+/**
+ * Created by Peeps on 10/26/16.
+ */
+public class PocketCSResource {
+
+}
